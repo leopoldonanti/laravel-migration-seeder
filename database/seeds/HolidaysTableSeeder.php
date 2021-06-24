5607adf2632c2367f43edbf7c1cf77c66eb6cd55
leopoldonanti <leonanti21@gmail.com>
2021-06-24 22:55:51 +0200

    seeder non partito

---
+++ database/seeds/HolidaysTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+use App\Holiday;
+use Illuminate\Database\Seeder;
+use League\CommonMark\Reference\Reference;
+
+class HolidaysTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+
+    {
+        $new_holiday = new Holiday();
+        $new_holiday->Price = 1000;
+        $new_holiday->Rooms = 4;
+        $new_holiday->Destination = "Fuerte Ventura";
+        $new_holiday->Type_of_Accomodation = "Bungalow";
+        $new_holiday->Season = "Summer";
+        $new_holiday->save();
+    }
+
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+@section('content')
+
+<main>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                vabene
+            </div>
+            
+        </div>
+    </div>
+</main>
+
+@endsection
